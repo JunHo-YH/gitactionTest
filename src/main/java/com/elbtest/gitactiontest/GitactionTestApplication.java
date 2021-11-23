0e4a41a8c0096485a042cc205ade11276976afaa
jackson <steady8@kakao.com>
2021-11-23 17:22:03 +0900

    init

---
+++ src/main/java/com/elbtest/gitactiontest/GitactionTestApplication.java
@@ -0,0 +1,13 @@
+package com.elbtest.gitactiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitactionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitactionTestApplication.class, args);
+    }
+
+}
