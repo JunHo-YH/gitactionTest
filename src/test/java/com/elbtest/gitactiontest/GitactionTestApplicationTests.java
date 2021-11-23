0d728c499d0e95c65209b6b49460ac6f1af04f1d
jackson <steady8@kakao.com>
2021-11-24 00:50:20 +0900

    git-action init

---
+++ src/test/java/com/elbtest/gitactiontest/GitactionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elbtest.gitactiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitactionTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
